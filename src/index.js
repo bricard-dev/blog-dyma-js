7297e975e490a43d5809423b75b83caf992af02b
Bastien Ricard <bastienricard@macbook-pro-de-bastien.home>
2022-03-18 18:59:06 +0100

    Add sidebar category display

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./assets/styles/styles.scss";
 import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
+const categoriesContainerElement = document.querySelector(".categories");
 
 const createArticles = (articles) => {
   const articlesDOM = articles.map((article) => {
@@ -63,6 +64,30 @@ const createArticles = (articles) => {
   });
 };
 
+const displayMenuCategories = (categoriesArr) => {
+  const liElements = categoriesArr.map((categoryElem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<li>${categoryElem[0]} ( <strong>${categoryElem[1]}</strong> )</li>`;
+    return li;
+  });
+
+  categoriesContainerElement.innerHTML = "";
+  categoriesContainerElement.append(...liElements);
+};
+
+const createMenuCategories = (articles) => {
+  const categories = articles.reduce((acc, article) => {
+    acc[article.category]
+      ? acc[article.category]++
+      : (acc[article.category] = 1);
+    return acc;
+  }, {});
+  const categoriesArr = Object.keys(categories).map((category) => {
+    return [category, categories[category]];
+  });
+  displayMenuCategories(categoriesArr);
+};
+
 const fetchArticle = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/article");
@@ -70,8 +95,8 @@ const fetchArticle = async () => {
     if (!Array.isArray(articles)) {
       articles = Array(articles);
     }
-    console.log(articles);
     createArticles(articles);
+    createMenuCategories(articles);
   } catch (e) {
     console.log("e : ", e);
   }
